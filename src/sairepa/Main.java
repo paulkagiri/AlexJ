7f02792173c4ad983b31b0a25a0bd1ee2962e550
jflesch <jflesch@bdd7d9b4-47d5-421e-87ec-140c7e9096ad>
2008-09-24 18:56:56 +0000

    Screw netbeans + add basic support for client files
    git-svn-id: https://jflesch.kwain.net/svn/sairepa@3 bdd7d9b4-47d5-421e-87ec-140c7e9096ad

--- src/sairepa/Main.java
+++ src/sairepa/Main.java
@@ -1,21 +1,61 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sairepa;
 
+import sairepa.model.ClientFile;
+import sairepa.gui.MainWindow;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+
 /**
- *
+ * Where everything starts.
  * @author jflesch
  */
 public class Main {
+  private ClientFile clientFile;
+  private MainWindow mainWindow;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Test");
+  private Main() throws Exception {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      System.err.println("WARNING - Can't set look'n'feel, because: "
+			 + e.toString());
+      System.err.println("Message: " + e.getMessage());
+      e.printStackTrace();
     }
 
+    try {
+      clientFile = new ClientFile("CLIENT.DAT");
+    } catch (ClientFile.InvalidClientFileException e) {
+      JOptionPane.showMessageDialog(null,
+				    "Fichier client invalide. Utilisation non-autorisée",
+				    "Fichier client invalide",
+				    JOptionPane.ERROR_MESSAGE);
+      throw e;
+    }
+
+    System.out.println("Client file:\n" + clientFile);
+
+    mainWindow = new MainWindow(this);
+  }
+
+  /**
+   * Called when the user asked to stop the application.
+   * Will take care of the confirmation dialogs / savings.
+   */
+  public void quit() {
+    System.exit(0);
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) throws Exception {
+    System.out.println("");
+    System.out.println("SAIsie des REgistres PAroissiaux");
+    System.out.println("");
+
+    new Main();
+  }
 }
---
+++ src/sairepa/model/ClientFile.java
@@ -0,0 +1,126 @@
+package sairepa.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Client files are used to make sure that the user is allowed to use
+ * this application and to know what he is allowed to do.
+ */
+public class ClientFile {
+  private String userName;
+  private String zipCode;
+  private String commune;
+
+  public ClientFile(String filePath) throws InvalidClientFileException {
+    this(new File(filePath));
+  }
+
+  public ClientFile(File file) throws InvalidClientFileException {
+    load(file);
+  }
+
+  public class InvalidClientFileException extends Exception {
+    public static final long serialVersionUID = 1;
+
+    protected InvalidClientFileException(String msg) {
+      super(msg);
+    }
+
+    protected InvalidClientFileException(String msg, Throwable cause) {
+      super(msg, cause);
+    }
+  }
+
+  protected void load(File file) throws InvalidClientFileException {
+    if (!file.exists()) {
+      throw new InvalidClientFileException("Client file doesn't exist.");
+    }
+
+    BufferedReader reader;
+
+    try {
+      reader = new BufferedReader(new FileReader(file));
+    } catch (IOException e) {
+      throw new InvalidClientFileException(
+	  "IOException when opening the client file", e);
+    }
+
+    long communeAndZipCode = -1;
+    String communeAndZip = null;
+    long userNameCode = -1;
+
+    try {
+      userName = reader.readLine();
+      communeAndZipCode = Long.parseLong(reader.readLine());
+      communeAndZip = reader.readLine();
+      userNameCode = Long.parseLong(reader.readLine());
+    } catch(NumberFormatException e) {
+      throw new InvalidClientFileException(
+          "Unable to read one of the codes", e);
+    } catch (IOException e) {
+      throw new InvalidClientFileException("IOException while reading", e);
+    } finally {
+      try {
+	reader.close();
+      } catch(IOException e) {
+	System.err.println("WARNING - Can't close the client file properly because: "
+			   + e.toString() + " : " + e.getMessage());
+	e.printStackTrace();
+      }
+    }
+
+    checkCode(userName, userNameCode, 49724, 28971);
+    checkCode(communeAndZip, communeAndZipCode, 64939, 62656);
+
+    String[] split = communeAndZip.split(" ");
+    this.zipCode = split[0];
+    this.commune = split[1];
+  }
+
+  protected void checkCode(String string, long code, final long a, final long b)
+      throws InvalidClientFileException {
+    long computed = getCode(string, a, b);
+
+    if (computed != code) {
+      throw new InvalidClientFileException("Invalid code.\n" +
+          "File: " + Long.toString(code) +
+          ", Computed: " + Long.toString(computed));
+    }
+  }
+
+  /**
+   * Weak DRM system.
+   * @return the code.
+   */
+  protected long getCode(String string, final long a, final long b) {
+    string = string.trim();
+
+    long code = 0;
+
+    for (int i = 0 ; i < string.length() ; i++) {
+      code += string.charAt(i);
+    }
+
+    return (code * a) - b;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public String getCommune() {
+    return commune;
+  }
+
+  public String toString() {
+    return ("  User: " + getUserName() + "\n"
+	    + "  Commune: " + getCommune() + ", " + getZipCode());
+  }
+}
